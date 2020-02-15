5e165c433e5add1b40b920a38b3aab725497c0ab
Roman Dolgyi <tosters@gmail.com>
2020-02-14 19:00:23 -0500

    Update kuber.groovy

--- vars/kuber.groovy
+++ vars/kuber.groovy
@@ -6,12 +6,14 @@ def branchExists(repo, branch) {
   return sh(returnStatus: true, script: "git ls-remote --exit-code --heads https://github.com/t0ster/${repo}.git ${branch}") == 0
 }
 
-def cicd(build) {
+def cicd(build, deploy = true) {
   def buildNumber = env.BUILD_NUMBER as int
   if (buildNumber > 1) milestone(buildNumber - 1)
   milestone(buildNumber)
 
-
+  if (env.TAG_NAME) {
+    deploy = false
+  }
   // def build = "ui"
   def branch = null
   if (env.CHANGE_BRANCH) {
@@ -66,65 +68,69 @@ def cicd(build) {
                   }
               }
           }
-          stage('Deploy') {
-              def kuberBranch = branchExists('kuber', branch) ? branch : 'master'
-              def namespace = (branch == 'master') ? 'stg' : branch
-              def patchOrg = """
-                  {
-                      "release": "kuber-${branch}",
-                      "repo": "https://github.com/t0ster/kuber.git",
-                      "branch" : "${kuberBranch}",
-                      "path": "charts/kuber-stack",
-                      "namespace": "${namespace}",
-                      "values": {
-                          "host": "${branch}.kuber.35.246.75.225.nip.io",
-                          "ui": {
-                              "image": {
-                                  "tag": "${containers['ui']['tag']}",
-                                  "pullPolicy": "Always",
-                                  "release": "kuber-${build}-${branch}-${BUILD_ID}"
-                              }
-                          },
-                          "functions": {
-                              "image": {
-                                  "tag": "${containers['functions']['tag']}",
-                                  "pullPolicy": "Always",
-                                  "release": "kuber-${build}-${branch}-${BUILD_ID}"
+          if (deploy) {
+            stage('Deploy') {
+                def kuberBranch = branchExists('kuber', branch) ? branch : 'master'
+                // def namespace = (branch == 'master') ? 'stg' : branch
+                def patchOrg = """
+                    {
+                        "release": "kuber-${branch}",
+                        "repo": "https://github.com/t0ster/kuber.git",
+                        "branch" : "${kuberBranch}",
+                        "path": "charts/kuber-stack",
+                        "namespace": "${namespace}",
+                        "values": {
+                            "host": "${branch}.kuber.35.246.75.225.nip.io",
+                            "ui": {
+                                "image": {
+                                    "tag": "${containers['ui']['tag']}",
+                                    "pullPolicy": "Always",
+                                    "release": "kuber-${build}-${branch}-${BUILD_ID}"
+                                }
+                            },
+                            "functions": {
+                                "image": {
+                                    "tag": "${containers['functions']['tag']}",
+                                    "pullPolicy": "Always",
+                                    "release": "kuber-${build}-${branch}-${BUILD_ID}"
 
-                              }
-                          }
-                      }
-                  }
-              """
-              echo patchOrg
-              def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: patchOrg, url: "http://deployer-kuber-deployer.kube-system"
-              def jsonObj = readJSON text: response.content
-              echo jsonObj['result']
-          }
+                                }
+                            }
+                        }
+                    }
+                """
+                echo patchOrg
+                def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: patchOrg, url: "http://deployer-kuber-deployer.kube-system"
+                def jsonObj = readJSON text: response.content
+                echo jsonObj['result']
+            }
+        }
     }
   }
 
-  stage('Functional Test') {
-    def image = containers['selenium']['image']
-    def tag = containers['selenium']['tag']
-    podTemplate(
-            containers: [
-                    containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave'),
-                    containerTemplate(name: 'selenium', alwaysPullImage: true, image: "${image}:${tag}", command: 'cat', ttyEnabled: true, envVars: [
-                        envVar(key: 'SELENIUM_HOST', value: 'zalenium'),
-                        envVar(key: 'BASE_URL', value: "http://${branch}.kuber.35.246.75.225.nip.io"),
-                        envVar(key: 'BUILD', value: "kuber-${build}-${branch}-${BUILD_ID}"),
-                    ])
-            ]
-    ) {
-      node(POD_LABEL) {
-        container('selenium') {
-            try {
-                sh 'pytest /app --verbose --junit-xml reports/tests.xml'
-            } finally {
-                junit testResults: 'reports/tests.xml'
-                echo "http://zalenium.35.246.75.225.nip.io/dashboard/?q=build:kuber-${build}-${branch}-${BUILD_ID}"
-            }
+  if (deploy) {
+    stage('Functional Test') {
+      def image = containers['selenium']['image']
+      def tag = containers['selenium']['tag']
+      podTemplate(
+              containers: [
+                      containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave'),
+                      containerTemplate(name: 'selenium', alwaysPullImage: true, image: "${image}:${tag}", command: 'cat', ttyEnabled: true, envVars: [
+                          envVar(key: 'SELENIUM_HOST', value: 'zalenium'),
+                          envVar(key: 'BASE_URL', value: "http://${branch}.kuber.35.246.75.225.nip.io"),
+                          envVar(key: 'BUILD', value: "kuber-${build}-${branch}-${BUILD_ID}"),
+                      ])
+              ]
+      ) {
+        node(POD_LABEL) {
+          container('selenium') {
+              try {
+                  sh 'pytest /app --verbose --junit-xml reports/tests.xml'
+              } finally {
+                  junit testResults: 'reports/tests.xml'
+                  echo "http://zalenium.35.246.75.225.nip.io/dashboard/?q=build:kuber-${build}-${branch}-${BUILD_ID}"
+              }
+          }
         }
       }
     }
